5493867e8fe537ee8aed483f025a180b5d72af8e
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-02 17:55:55 +0200

    zadanie_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const listItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" 
+    alt="${galleryItem.description}"/></a></li>`
+  )
+  .join("");
+gallery.innerHTML = listItems;
 
-console.log(galleryItems);
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const instance =
+    basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const listItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li><a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" 
+    alt="${galleryItem.description}"/></a></li>`
+  )
+  .join("");
+gallery.innerHTML = listItems;
